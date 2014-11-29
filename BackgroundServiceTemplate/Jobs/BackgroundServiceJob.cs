7061b275ffb25afc7baa7b8acbd002e9afae4b09
Allan Oliveira <r9oliveira@gmail.com>
2014-11-29 13:16:47 +0800

    Create BackgroundServiceJob base class.

---
+++ BackgroundServiceTemplate/Jobs/BackgroundServiceJob.cs
@@ -0,0 +1,20 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackgroundServiceTemplate.Interfaces
+{
+    public abstract class BackgroundServiceJob : IJob
+    {
+        public abstract TriggerBuilder GetSchedule(TriggerBuilder triggerBuilder);
+        protected abstract void ExecuteInner(IJobExecutionContext context);
+
+        public void Execute(IJobExecutionContext context)
+        {
+            ExecuteInner(context);
+        }
+    }
+}
