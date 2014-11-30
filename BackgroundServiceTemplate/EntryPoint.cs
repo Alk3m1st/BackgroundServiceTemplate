69fd28cac81cb7dbd737931647f0dd3f7f66f34a
Allan Oliveira <r9oliveira@gmail.com>
2014-12-01 00:35:47 +0800

    Added EntryPoint class to separate boilerplate code.

---
+++ BackgroundServiceTemplate/EntryPoint.cs
@@ -0,0 +1,48 @@
+﻿using Autofac;
+using Autofac.Extras.Quartz;
+using BackgroundServiceTemplate.Modules;
+using BackgroundServiceTemplate.Services;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace BackgroundServiceTemplate
+{
+    public class EntryPoint<T> where T : class, IServiceScheduler
+    {
+        protected IContainer Container { get; set; }
+
+        public void Init(Action<ContainerBuilder> registerDependencies)
+        {
+            Trace.WriteLine("Register dependencies with Autofac...");
+            var builder = new ContainerBuilder();
+            builder.RegisterModule(new QuartzAutofacFactoryModule());
+            builder.RegisterModule(new AutofacJobsModule(typeof(T).Assembly));
+            builder.RegisterType<T>().AsSelf().SingleInstance();
+            registerDependencies(builder);
+            Container = builder.Build();
+
+            HostFactory.Run(x =>
+            {
+                x.Service<T>(s =>
+                {
+                    s.ConstructUsing(name => Container.Resolve<T>());
+                    s.WhenStarted(ss => ss.Start());
+                    s.WhenStopped(ss => ss.Stop());
+                });
+                x.RunAsLocalSystem();
+
+                x.SetDescription("Background service Topshelf host");
+                x.SetDisplayName("BackgroundServiceTemplate");
+                x.SetServiceName("BackgroundServiceTemplate");
+                x.SetInstanceName(Environment.MachineName);
+
+                x.EnableServiceRecovery(rc => rc.RestartService(1));    // Restart the service after 1 minute if it crashed
+            });
+        }
+    }
+}
--- BackgroundServiceTemplate/Program.cs
+++ BackgroundServiceTemplate/Program.cs
@@ -14,34 +14,15 @@ namespace BackgroundServiceTemplate
 {
     public class Program
     {
-        public static IContainer Container { get; set; }
-
         public static void Main()
         {
-            Trace.WriteLine("Register dependencies with Autofac...");
-            var builder = new ContainerBuilder();
-            builder.RegisterModule(new QuartzAutofacFactoryModule());
-            builder.RegisterModule(new AutofacJobsModule(typeof(ServiceScheduler).Assembly));   // Change with type T
-            builder.RegisterType<ServiceScheduler>().AsSelf().SingleInstance();
-            Container = builder.Build();
+            var entryBase = new EntryPoint<ServiceScheduler>();
+            entryBase.Init(RegisterDependencies);
+        }
 
-            HostFactory.Run(x =>
-            {
-                x.Service<ServiceScheduler>(s =>
-                {
-                    s.ConstructUsing(name => Container.Resolve<ServiceScheduler>());
-                    s.WhenStarted(ss => ss.Start());
-                    s.WhenStopped(ss => ss.Stop());
-                });
-                x.RunAsLocalSystem();
-
-                x.SetDescription("Background service Topshelf host");
-                x.SetDisplayName("BackgroundServiceTemplate");
-                x.SetServiceName("BackgroundServiceTemplate");
-                x.SetInstanceName(Environment.MachineName);
-
-                x.EnableServiceRecovery(rc => rc.RestartService(1));    // Restart the service after 1 minute if it crashed
-            });
+        public static void RegisterDependencies(ContainerBuilder builder)
+        {
+            // TODO: Add further dependencies
         }
     }
 }
